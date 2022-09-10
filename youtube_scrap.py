8cca2d1e20ef3649438195c416f872c38aaf5ef0
subhajit bera <subhajit16.93@gmail.com>
2022-09-10 10:36:33 +0530

    runtime

--- youtube_scrap.py
+++ youtube_scrap.py
@@ -9,6 +9,7 @@ import os
 from mysql.connector import  connect
 import base64
 import pymongo
+import threading
 
 drive = GoogleDrive(gauth)
 
@@ -30,7 +31,7 @@ client = pymongo.MongoClient("mongodb+srv://subho9717:subho9717@ineuron.8npdrfs.
 
 def video_data(url_video, final_url):
     # video link
-    print(url_video,final_url)
+    # print(url_video,final_url)
     video_link1 = final_url
     video_author = YouTube(final_url).author
 
@@ -64,93 +65,98 @@ def video_data(url_video, final_url):
 
             box.append([name, comment])
 
-    # df = pd.DataFrame(
-    #     {'Author': video_author,   'Commenter_Name': [i[0] for i in box],
-    #      'Comment': [i[1] for i in box]})
+    
 
-    ##########################################Image
-    imglink = thumbnails
-    image = requests.get(imglink).content
-    img_title = str(title).replace("?", "_").replace("|", "_").replace("/", "_").replace("'\'", "_").replace("~", "_").replace("`", "_").replace("!", "_").replace("@", "_").replace("#", "_").replace("$", "_").replace("%", "_").replace("^", "_").replace("&", "_").replace("*", "_").replace("(", "_").replace(")", "_").replace("-", "_").replace("+", "_").replace("=", "_").replace(":", "_").replace(";", "_").replace('"""', "_").replace("'", "_").replace(",", "_").replace("<", "_").replace(">", "_")
-    # imgtitle = r"media/images/" + img_title + '.jpg'
-    imgtitleg ='media/images/'+ img_title + '.jpg'
-    # with open(imgtitle, "wb") as file:
-    #     file.write(image)
+    def mongo(thumbnails,title,mqldata1):
 
-    # imagefolder = '1vKXX5hwd2K9FWH2l9gwrXebN0hKosnum'
-    # file5 = drive.CreateFile({
-    #     'title':url_video+'.jpg',
-    #     'parents': [{'id': imagefolder}]
-    # })
-    with open(imgtitleg, "wb") as file:
-        file.write(image)
-
-    dir = r"media/images"
-    all_files = os.listdir(dir)
-    for f in all_files:
-        path = os.path.join(dir, f)
-        with open(path, "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read())
-            # print(encoded_string)
-            for i in box:
-                for j in mqldata1:
-                    data = {'Author': video_author,
-                            'title':j[0].replace(" ", "_").replace("?", "_").replace("|", "_").replace("/", "_").replace("'\'", "_").replace("~", "_").replace("`", "_").replace("!", "_").replace("@", "_").replace("#", "_").replace("$", "_").replace("%", "_").replace("^", "_").replace("&", "_").replace("*", "_").replace("(", "_").replace(")", "_").replace("-", "_").replace("+", "_").replace("=", "_").replace(":", "_").replace(";", "_").replace('"""', "_").replace("'", "_").replace(",", "_").replace("<", "_").replace(">", "_"),
-                            'Commenter_Name': i[0],
-                            'Comment': i[1],
-                            'thumbnail':encoded_string,
-                            'Video_watch_url': url_video
-                            }
-                    db = client['YouTube_Video_Data']
-                    coll = db['YouTubers_table']
-                    coll.insert_one(data)
-
-
-    # file5.SetContentFile(imgtitleg)
-    # file5.Upload()
-
-    ######################################################
-    videofolder = '1qDl8vUE3qy0yBhxTIz1ztfNQ3KDsWVB4'
-    youtube_1 = YouTube(final_url)
-    # # print(youtube_1.thumbnail_url)
-    # videos_1 = youtube_1.streams.filter(file_extension='mp4')
-    videos_1 = youtube_1.streams.all()
-    # print(videos_1)
-    vid = list(enumerate(videos_1))
-    strm = 0
-    videos_1[strm].download("media/video/")
-
-    dir = r"media/video"
-    all_files = os.listdir(dir)
-
-    for f in all_files:
-        path = os.path.join(dir, f)
-        vfname = str(f).replace(" ", "_").replace("?", "_").replace("|", "_").replace("/", "_").replace("'\'", "_").replace("~", "_").replace("`", "_").replace("!", "_").replace("@", "_").replace("#", "_").replace("$", "_").replace("%", "_").replace("^", "_").replace("&", "_").replace("*", "_").replace("(", "_").replace(")", "_").replace("-", "_").replace("+", "_").replace("=", "_").replace(":", "_").replace(";", "_").replace('"""', "_").replace("'", "_").replace(",", "_").replace("<", "_").replace(">", "_")
-        # print(vfname)
-        file6 = drive.CreateFile({
-            'title':vfname,
-            # 'title': url_video+'.'+str(vfname).split('.')[1],
-            'parents': [{'id': videofolder}]
-        })
-        # file6.SetContentFile(path)
-        # file6.Upload()
-
-    print('successfully')
-    for i in mqldata1:
+        ##########################################Image
+        imglink = thumbnails
+        image = requests.get(imglink).content
+        img_title = str(title).replace("?", "_").replace("|", "_").replace("/", "_").replace("'\'", "_").replace("~", "_").replace("`", "_").replace("!", "_").replace("@", "_").replace("#", "_").replace("$", "_").replace("%", "_").replace("^", "_").replace("&", "_").replace("*", "_").replace("(", "_").replace(")", "_").replace("-", "_").replace("+", "_").replace("=", "_").replace(":", "_").replace(";", "_").replace('"""', "_").replace("'", "_").replace(",", "_").replace("<", "_").replace(">", "_")
+        imgtitleg ='media/images/'+ img_title + '.jpg'
         
-        sqlq1 = "INSERT INTO YouTubers_Table(YouTubers_Name ,Video_Link ,Video_Likes , Number_Of_Comments ,Title_Of_Video ,Thumbnail_Of_Video_Link,Video_watch_url) values (%s,%s,%s,%s,%s,%s,%s)"
-        val = (video_author, video_link1, i[1], i[2], i[0], imglink, url_video)
+        with open(imgtitleg, "wb") as file:
+            file.write(image)
 
-        cursor.execute(sqlq1, val)
-        conn.commit()
-    # return url_video
+        dir = r"media/images"
+        all_files = os.listdir(dir)
+        for f in all_files:
+            path = os.path.join(dir, f)
+            with open(path, "rb") as image_file:
+                encoded_string = base64.b64encode(image_file.read())
+                # print(encoded_string)
+                for i in box:
+                    for j in mqldata1:
+                        data = {'Author': video_author,
+                                'title':j[0].replace(" ", "_").replace("?", "_").replace("|", "_").replace("/", "_").replace("'\'", "_").replace("~", "_").replace("`", "_").replace("!", "_").replace("@", "_").replace("#", "_").replace("$", "_").replace("%", "_").replace("^", "_").replace("&", "_").replace("*", "_").replace("(", "_").replace(")", "_").replace("-", "_").replace("+", "_").replace("=", "_").replace(":", "_").replace(";", "_").replace('"""', "_").replace("'", "_").replace(",", "_").replace("<", "_").replace(">", "_"),
+                                'Commenter_Name': i[0],
+                                'Comment': i[1],
+                                'thumbnail':encoded_string,
+                                'Video_watch_url': url_video
+                                }
+                        db = client['YouTube_Video_Data']
+                        coll = db['YouTubers_table']
+                        coll.insert_one(data)
+
+
+    def video1(final_url):
+
+        ######################################################
+        videofolder = '1qDl8vUE3qy0yBhxTIz1ztfNQ3KDsWVB4'
+        youtube_1 = YouTube(final_url)
+        # # print(youtube_1.thumbnail_url)
+        # videos_1 = youtube_1.streams.filter(file_extension='mp4')
+        videos_1 = youtube_1.streams.all()
+        # print(videos_1)
+        vid = list(enumerate(videos_1))
+        strm = 0
+        videos_1[strm].download("media/video/")
+
+        dir = r"media/video"
+        all_files = os.listdir(dir)
+
+        for f in all_files:
+            path = os.path.join(dir, f)
+            vfname = str(f).replace(" ", "_").replace("?", "_").replace("|", "_").replace("/", "_").replace("'\'", "_").replace("~", "_").replace("`", "_").replace("!", "_").replace("@", "_").replace("#", "_").replace("$", "_").replace("%", "_").replace("^", "_").replace("&", "_").replace("*", "_").replace("(", "_").replace(")", "_").replace("-", "_").replace("+", "_").replace("=", "_").replace(":", "_").replace(";", "_").replace('"""', "_").replace("'", "_").replace(",", "_").replace("<", "_").replace(">", "_")
+            # print(vfname)
+            file6 = drive.CreateFile({
+                'title':vfname,
+                # 'title': url_video+'.'+str(vfname).split('.')[1],
+                'parents': [{'id': videofolder}]
+            })
+            # file6.SetContentFile(path)
+            # file6.Upload()
+
+        print('successfully')
+    
+    def mysql(mqldata1,thumbnails,url_video):
+        for i in mqldata1:
+            
+            sqlq1 = "INSERT INTO YouTubers_Table(YouTubers_Name ,Video_Link ,Video_Likes , Number_Of_Comments ,Title_Of_Video ,Thumbnail_Of_Video_Link,Video_watch_url) values (%s,%s,%s,%s,%s,%s,%s)"
+            val = (video_author, video_link1, i[1], i[2], i[0], thumbnails, url_video)
+
+            cursor.execute(sqlq1, val)
+            conn.commit()
+        
+    print(final_url)
+    t1 = threading.Thread(target=mongo,args=(thumbnails,title,mqldata1))
+    t2 = threading.Thread(target=mysql,args=(mqldata1,thumbnails,url_video))
+    t3 = threading.Thread(target=video1,args=(final_url,))
+
+    t1.start()
+    t3.start()
+    t2.start()
+
+    t1.join()
+    t3.join()
+    t2.join()
 
 def get_all_video_url(video_url):
 
     try:
-        print(video_url)
+        # print(video_url)
         
-        print('ok')
+        # print('ok')
 
         db = client['YouTube_Video_Data']
         col = db['YouTubers_table']
@@ -159,7 +165,7 @@ def get_all_video_url(video_url):
         c = Channel(video_url)
         count = 0
         for r in c:
-            print(r)
+            # print(r)
             dir = r"./media/images"
             all_files = os.listdir(dir)
             for f in all_files:
@@ -174,7 +180,7 @@ def get_all_video_url(video_url):
 
             final_url = str(r)
             url_video = str(r)[-11:]
-            print(url_video, final_url,count)
+            # print(url_video, final_url,count)
             data = video_data(url_video, final_url)
 
             if count == 5:
@@ -189,4 +195,4 @@ def get_all_video_url(video_url):
         print(e)
     conn.close()
 
-# get_all_video_url('https://www.youtube.com/c/HiteshChoudharydotcom/videos')
+get_all_video_url('https://www.youtube.com/c/HiteshChoudharydotcom/videos')
